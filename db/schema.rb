068b6791e21ad63831e76f3080941fedbae631fb
benjamin montet <benjamin.montet@gmail.com>
2024-11-26 14:48:46 +0100

    fix migration problem with active storage (double)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_11_26_120431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,12 +42,83 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_120431) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  create_table "appointments", force: :cascade do |t|
+    t.date "date"
+    t.string "status"
+    t.bigint "match_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "shelter_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["match_id"], name: "index_appointments_on_match_id"
+    t.index ["shelter_id"], name: "index_appointments_on_shelter_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_26_102417) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  create_table "conversations", force: :cascade do |t|
+    t.bigint "match_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "shelter_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["match_id"], name: "index_conversations_on_match_id"
+    t.index ["shelter_id"], name: "index_conversations_on_shelter_id"
+    t.index ["user_id"], name: "index_conversations_on_user_id"
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pet_id"], name: "index_matches_on_pet_id"
+    t.index ["user_id"], name: "index_matches_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.integer "age"
+    t.string "category"
+    t.text "description"
+    t.string "gender"
+    t.boolean "ok_cat"
+    t.boolean "ok_dog"
+    t.boolean "ok_kid"
+    t.boolean "affectionate"
+    t.text "behavior"
+    t.string "size"
+    t.text "needs"
+    t.text "environment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shelters", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.float "longitude"
+    t.float "latitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "spacies", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -58,9 +128,39 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_102417) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "age"
+    t.string "gender"
+    t.string "lifestyle"
+    t.boolean "remote_work"
+    t.string "address"
+    t.string "green_space"
+    t.integer "household_size"
+    t.integer "time_for_pet"
+    t.boolean "daily_walk"
+    t.integer "pet_budget"
+    t.boolean "have_children"
+    t.boolean "have_cat"
+    t.boolean "have_dog"
+    t.boolean "have_other_pet"
+    t.text "description"
+    t.boolean "can_adopt_dog"
+    t.boolean "can_adopt_cat"
+    t.boolean "can_adopt_nac"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "appointments", "matches"
+  add_foreign_key "appointments", "shelters"
+  add_foreign_key "appointments", "users"
+  add_foreign_key "conversations", "matches"
+  add_foreign_key "conversations", "shelters"
+  add_foreign_key "conversations", "users"
+  add_foreign_key "matches", "pets"
+  add_foreign_key "matches", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
