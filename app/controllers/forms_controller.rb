82443fba78ca77899ee08f31edfe303f7f13682e
Chloé Bretnacher <chloe.bretnacher@gmail.com>
2024-11-27 16:40:23 +0100

    formulaire

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,27 @@
+class FormsController < ApplicationController
+
+  def tobegin
+    @user = current_user
+  end
+
+  def aboutyou
+    @user = current_user
+  end
+
+  def welldone
+    @user = current_user
+  end
+
+  def daily
+    @user = current_user
+  end
+
+  def almostdone
+    @user = current_user
+  end
+
+  def adoptionproject
+    @user = current_user
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_26_155755) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_27_103410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -137,7 +137,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_155755) do
     t.string "lifestyle"
     t.boolean "remote_work"
     t.string "address"
-    t.string "green_space"
     t.integer "household_size"
     t.integer "time_for_pet"
     t.boolean "daily_walk"
@@ -151,6 +150,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_155755) do
     t.boolean "can_adopt_cat"
     t.boolean "can_adopt_nac"
     t.bigint "shelter_id"
+    t.boolean "has_green_space"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["shelter_id"], name: "index_users_on_shelter_id"
