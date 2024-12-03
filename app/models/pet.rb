e233d7164d6daecab13044509fed947e0c4477cf
Etienne Verschave <etienneverschave@gmail.com>
2024-12-03 14:44:27 +0100

    add filter

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,4 +3,10 @@ class Pet < ApplicationRecord
   belongs_to :shelter
   has_many :matchs, dependent: :destroy
   has_many_attached :photos
+
+  private
+
+  def self.unmatched_for(user)
+    where.not(id: Match.where(user_id: user.id).select(:pet_id))
+  end
 end
