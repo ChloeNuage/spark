d1cd97387767d6ad935df96f99db2e26eab5afed
eliot delmas <delmaseliot@gmail.com>
2024-12-04 16:43:12 +0100

    Booking turbostream

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,16 @@
 class Appointment < ApplicationRecord
+
+  after_create_commit -> {
+    broadcast_append_to "match_#{match.id}_appointments",
+    partial: "appointments/appointment",
+    target: "appointments"
+  }
+  after_destroy_commit -> {
+    broadcast_remove_to "match_#{match.id}_appointments",
+    partial: "appointments/appointment",
+    target: "appointments"
+  }
+
   belongs_to :match
   belongs_to :user
   belongs_to :shelter
