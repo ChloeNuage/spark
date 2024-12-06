a79b01d17eb6b2c090a25bf69e285158aee3a725
Etienne Verschave <etienneverschave@gmail.com>
2024-12-06 10:00:41 +0100

    Fix form input, rajout de la valeur par default

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,14 @@ class User < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_one_attached :photo
   belongs_to :shelter, optional: true
+  
+  attribute :remote_work?, :boolean, default: false
+  attribute :green_space, :boolean, default: false
+  attribute :household_size, :integer, default: 1
+  attribute :time_for_pet, :integer, default: 0
+  attribute :daily_walk?, :boolean, default: false
+  attribute :pet_budget, :integer, default: 0
+  attribute :description, :string, default: ''
 
   private
 
